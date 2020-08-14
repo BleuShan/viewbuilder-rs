2444b81f320548f9100832601f7e97f1d0a74347
Philippe Lafontaine <lafontaine.philippe@gmail.com>
2020-08-14 14:00:12 -0400

    docs: add doc preamble

--- viewbuilder-core/src/lib.rs
+++ viewbuilder-core/src/lib.rs
@@ -1,5 +1,8 @@
+//! Core viewbuilder abstractions.
+
 #![forbid(future_incompatible)]
 #![warn(
+    missing_docs,
     missing_debug_implementations,
     nonstandard_style,
     rust_2018_idioms,
--- viewbuilder/src/lib.rs
+++ viewbuilder/src/lib.rs
@@ -1,5 +1,11 @@
+//! An experiment in rendering ui views.
+//!
+//! This project is a study the implementation of a declarative
+//! user interface runtime.
+
 #![forbid(future_incompatible)]
 #![warn(
+    missing_docs,
     missing_debug_implementations,
     nonstandard_style,
     rust_2018_idioms,
